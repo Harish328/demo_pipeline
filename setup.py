20c58e91649359eae5c47e625ffa050430d25c1b
hsdfhjkh d@ksdfghk .fdshsdk <dsadsfgjhgdsf@g>
2024-04-17 11:05:43 +0530

    pakage creation

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name="census-income", 
+      version="0.1",
+      author="harish",
+      author_email="gupta.harish328@gmail.com",
+      packages=find_packages(),
+      install_requires=[pandas,numpy,flask]
+      )
